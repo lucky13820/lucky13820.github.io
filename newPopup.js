df2b99f3bcf31e21d5b281b69aafc93469bc95b5
Ryan Yao <hello@ryanyao.design>
2024-10-31 14:39:11 -0700

    newpopup

---
+++ newPopup.js
@@ -0,0 +1,57 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Helper function to get the value of a URL parameter
+  function getQueryParam(param) {
+    var urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+  }
+
+  // Function to change the popup text and price based on the promo type
+  function setPopupAndPrice(promoType) {
+    var popupText;
+    var price;
+    var planName;
+
+    // Convert promoType to lowercase for case-insensitive comparison
+    promoType = promoType.toLowerCase();
+
+    switch (promoType) {
+      case "39":
+        price = "$39";
+        break;
+      case "29":
+        price = "$28.65";
+        break;
+      case "19":
+        price = "$19";
+        break;
+      case "9":
+        price = "$9";
+        break;
+      default:
+        return false;
+    }
+    return false;
+  }
+
+  // Get the promo value from the URL
+  var promoValue = getQueryParam("promo");
+
+  // If promoValue is present and valid, call setPopupAndPrice with the promo value
+  if (promoValue) {
+    setPopupAndPrice(promoValue);
+  }
+
+  const weightLossNumber = document.querySelector("[data-weightloss-number]");
+
+  let weightLossStorage = localStorage.getItem("quiz_prediction");
+  if (weightLossStorage) {
+    if (weightLossNumber) {
+      weightLossNumber.textContent = weightLossStorage;
+    }
+  } else {
+    if (weightLossNumber) {
+      weightLossNumber.parentElement.innerHTML =
+        "Unlock access to GLP-1 weight loss";
+    }
+  }
+});
