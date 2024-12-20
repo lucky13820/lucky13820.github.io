29661b3e32657928f4cf91cdaab62b8a57f3eaad
Ryan Yao <hello@ryanyao.design>
2024-12-20 11:49:27 -0800

    remove price form

--- price-quiz-v2.2.js
+++ price-quiz-v2.2.js
@@ -5,7 +5,6 @@ let eligibleStates;
 
 document.addEventListener("DOMContentLoaded", () => {
   initializeSwiper();
-  addPriceFormListeners();
   trackSurveyStartToSimplifi();
   initializeBraze();
   updateContent();
