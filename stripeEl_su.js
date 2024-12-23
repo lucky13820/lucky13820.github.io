b85e7ac67fc0829ffb93deddf9fb7ed12207b170
Ryan Yao <hello@ryanyao.design>
2024-12-23 14:33:33 -0800

    promoCode

--- stripeEl_su.js
+++ stripeEl_su.js
@@ -77,6 +77,8 @@ const getQueryParam = (paramName) => {
   return new URLSearchParams(window.location.search).get(paramName) || null;
 };
 
+const promoCode = getQueryParam("promo");
+
 checkStatus();
 
 document
