f6e58e2d57f3c558842282c4b6a5395e3578b3f3
Ryan Yao <hello@ryanyao.design>
2024-12-31 14:21:55 -0800

    revert

--- buy_compounded_katalys.js
+++ buy_compounded_katalys.js
@@ -1,4 +1,3 @@
-import { trackEvent } from 'https://sunrise-webflow.vercel.app/scripts/track_event.js';
 
 const isStaging = window.location.href.includes('webflow')
 const urlParams = new URLSearchParams(window.location.search)
@@ -114,11 +113,82 @@ if (promo?.toLowerCase() === 'newyear199') {
     }
 }
 
-// Track Add to Cart when the DOM is fully loaded
-document.addEventListener('DOMContentLoaded', () => {
-    trackEvent({
-        capiEvent: 'CompoundedSemaglutideAddToCart',
-        gtmTrigger: 'begin_checkout_v2',
-        gtmEventName: 'CompoundedSemaglutideAddToCart',
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie
+function readCookies() {
+    const cookieMap = {}
+    const cookies = document.cookie.split('; ')
+    for (const cookiePair of cookies) {
+        const [key, ...value] = cookiePair.split('=')
+        // The cookie value may have equal signs in it, so re-insert them here
+        cookieMap[key] = value.join('=')
+    }
+    return cookieMap
+}
+
+async function getUserData() {
+    // Grab the Facebook Click and Browser IDs from the user's cookie
+    const { _fbc: fbc, _fbp: fbp } = readCookies()
+    const { userAgent } = window.navigator
+
+    // Use Amazon to get the user's IP address
+    // See https://ipdata.co/blog/how-to-get-the-ip-address-in-javascript/
+    const res = await fetch('https://checkip.amazonaws.com/')
+    const ipAddress = (await res.text()).trim()
+    return { fbc, fbp, userAgent, ipAddress }
+}
+
+function getBaseApiUrl() {
+    return isStaging ? 'https://api.staging.findsunrise.com' : 'https://api.findsunrise.com'
+}
+
+async function sendAddToCartEvent() {
+    const { fbc, fbp, userAgent, ipAddress } = await getUserData()
+    const eventId = crypto.randomUUID()
+
+    // Push event to GTM so we can forward this to a pixel
+    window.dataLayer.push({
+        event: 'begin_checkout_v2',
+        event_id: eventId,
+        event_name: 'CompoundedSemaglutideAddToCart',
+        client_ip_address: ipAddress,
+        client_user_agent: userAgent,
+        fbc,
+        fbp,
     })
-})
+
+    // Parse the quiz answers from localStorage
+    const quizAnswers = JSON.parse(localStorage.getItem('quizAnswers') || '{}')
+
+    // Prepare the request body
+    const body = {
+        eventName: 'CompoundedSemaglutideAddToCart',
+        eventId,
+        email: quizAnswers.email || '',
+        phone: quizAnswers.phone || '',
+        fbClickId: fbc,
+        fbBrowserId: fbp,
+        ipAddress,
+        userAgent,
+    }
+
+    // Send the CAPI request
+    const res = await fetch(`${getBaseApiUrl()}/api/conversions`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+    }).catch((error) => {
+        if (isStaging) {
+            console.log('Failed to send CAPI request:', body)
+            console.error('Error:', error)
+        }
+    })
+
+    if (isStaging) {
+        console.log('Sent CAPI request:', { res, body })
+    }
+}
+
+// Call the function when the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', sendAddToCartEvent)
