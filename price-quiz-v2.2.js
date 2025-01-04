f99b124add0f7905017151b9055c50e42d887b60
Ryan Yao <hello@ryanyao.design>
2025-01-03 16:26:21 -0800

    updates again

--- price-quiz-v2.2.js
+++ price-quiz-v2.2.js
@@ -1078,7 +1078,6 @@ function toggleSlide(swiper, shouldRemove, slideEventValue) {
 }
 
 function handleNoneCheckbox() {
-  //
   const checkboxes = document.querySelectorAll('input[type="checkbox"]');
   
   checkboxes.forEach(checkbox => {
